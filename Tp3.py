2fb084908e5d868e4005990622349be0cb88cb94
martinj <martinj@educ.college-montreal.qc.ca>
2022-10-26 09:50:38 -0400

    fix text

--- Tp3.py
+++ Tp3.py
@@ -70,16 +70,16 @@ def fight():
             input(
                 "Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire. "
                 "Dans ce cas, le nivea de vie de l’usager est augmenté de la force de l’adversaire. Une défaite a lieu "
-                "lorsque la valeur du dé lancé par l’usager est inférieure ou égale à la force de l’adversaire.  Dans "
-                "ce "
-                "cas, le niveau de vie de l’usager est diminué de la force de l’adversaire. La partie se termine "
-                "lorsque "
+                "lorsque la valeur du dé lancé par l’usager est "
+                "inférieure ou égale à la force de l’adversaire.  Dans ce "
+                "cas, le niveau de vie de l’usager est diminué de la force"
+                " de l’adversaire. La partie se termine lorsque "
                 "les points de vie de l’usager tombent sous 0. L’usager peut combattre ou éviter chaque adversaire, "
                 "dans le cas de l’évitement, il y a une pénalité de 1 point de vie.\nEntrez quelque chose pour "
                 "continuer\n\n")
 
         else:
-            rejouer = 2
+            rejouer, fighting = 2, False
 
 
 while rejouer != 2:
